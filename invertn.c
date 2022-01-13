a5806aa8679f3f269e6d1ea5c327e0395b5b341a
lisbon <16432726+honeydatax@users.noreply.github.com>
2022-01-13 19:34:22 +0000

    Add files via upload

---
+++ invertn.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int neg(a,n)
+int a;
+int n;
+{
+	return n-a ;
+}
+int main(){
+	int i=0;
+	for(i=0;i<15;i++){
+		printf("%d<<%d\n",i,neg(i,12));
+	}
+	return 0;
+}
---
+++ len.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int strslen(s)
+char *s;
+{
+	int pos=0;
+	while(s[pos]!=0){
+		pos++;
+	}
+	return pos;
+}
+
+int main(){
+	char *hello="hello world\n";
+	printf("%s  %d\n",hello,strslen(hello));
+	return 0;
+}
